48e9ae997e449e05209e68037bc4aa9eb8a4a60d
Epyon01P <joannes.laveyne@gmail.com>
2023-04-11 22:57:49 +0200

    Cleanup

--- P1-dongle.ino
+++ P1-dongle.ino
@@ -1,7 +1,6 @@
 #include "rom/rtc.h"
 #include "M5Atom.h"
 #include <WiFi.h>
-#include <WiFiMulti.h>
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 #include <DNSServer.h>
@@ -14,7 +13,6 @@
 #include "WebRequestHandler.h"
 #include <PubSubClient.h>
 #include "time.h"
-//#include "tls.hpp"
 #include "FS.h"
 #include <Update.h>
 #include "ArduinoJson.h"
@@ -174,12 +172,6 @@ void setup(){
   }
   delay(100);
   //your other setup stuff...
-  /*TEMP*/
-  wifiSTA = true;
-  wifi_ssid = "Aether";
-  wifi_password = "RaidillondelEauRouge0x03";
-  beta_fleet = true;
-  /*ENDTEMP*/
   if(wifiSTA){
     syslog("WiFi mode: station", 1);
     WiFi.mode(WIFI_STA);
@@ -369,51 +361,4 @@ void loop(){
     }
     splitTelegram(s);
   }
-}
-
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\r\n", dirname);
-
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("- failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println(" - not a directory");
-        return;
-    }
-
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.path(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("\tSIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
-}
-
-void readFile(fs::FS &fs, const char * path){
-    Serial.printf("Reading file: %s\r\n", path);
-
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed to open file for reading");
-        return;
-    }
-
-    Serial.println("- read from file:");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-    file.close();
 }
