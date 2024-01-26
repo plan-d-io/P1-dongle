fad9390e3200f4c2ad0f6a8e578ab62eebedfe7a
Epyon01P <joannes.laveyne@gmail.com>
2024-01-26 14:13:47 +0100

    Further refine spurious reading detection

--- P1-dongle.ino
+++ P1-dongle.ino
@@ -27,7 +27,7 @@
 #define HWSERIAL Serial1
 #define TRIGGER 25 //Pin to trigger meter telegram request
 
-unsigned int fw_ver = 221;
+unsigned int fw_ver = 222;
 
 //General global vars
 Preferences preferences;
